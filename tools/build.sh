062ca80f8023e5cfd43e93c5e495467e6a5ad313
Ernesto Alejo <ernestokarim@gmail.com>
2016-06-17 23:49:24 +0200

    Concatenate dist files better.

--- tools/build.sh
+++ tools/build.sh
@@ -11,14 +11,14 @@ for lang in "${langs[@]}"
 do
   echo "--- Build lang $lang"
   java -jar vendor/closure-compiler/build/compiler.jar \
-    --js_output_file dist/datepicker-$lang.js \
+    --js_output_file dist/datepicker-$lang.min.js \
     --only_closure_dependencies \
     --language_in ECMASCRIPT5_STRICT \
     --closure_entry_point altipla.InputDatePicker \
     --compilation_level ADVANCED \
     --warning_level VERBOSE \
     --generate_exports \
-    --output_wrapper "\n(function() {%output%}).call(this);" \
+    --output_wrapper "(function() {%output%}).call(this);" \
     --jscomp_error checkTypes \
     --jscomp_error checkVars \
     --jscomp_error deprecated \
